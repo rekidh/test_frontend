3f98cc3e9eab53bc3d5352b55f112e87109fc285
rekidh <89791981+rekidh@users.noreply.github.com>
2023-01-25 14:33:19 +0700

    first commit

--- src/component/LogoBoxs.jsx
+++ src/component/LogoBoxs.jsx
@@ -1,32 +1,24 @@
 import React from 'react'
-import { useEffect,useState } from 'react'
+import {useState } from 'react'
+import {useDrag} from 'react-use-gesture'
 
 
 export default function LogoBoxs(prop) {
 
-  const [mouse,setMouse]=useState({})
+  const [mouse,setMouse]=useState({x:0,y:0})
   const text=prop.logo?[...prop.logo]: Array.from('tulis sesuatu')
-
-  const handleMouse=(e)=>{
-    let x = e.clientX
-    let y = e.clientY
-    setMouse({x,y})
-  }
-
-  const handleClick =(e)=>{
-    e.target.style=`
-    position: absolute;
-    transform: translate3d(${e.clientX}px,${e.clientY}px,)
-    `
-    handleMouse(e)
-    console.log(mouse) 
-    }
+  const handleMouse= useDrag((e)=>{
+    setMouse({
+      x:e.offset[0],
+      y:e.offset[1]
+    })
+  })
 
   
   return (
     <div className={prop.className} >
-      <div className='center h-md' id='text-container' /*style={{backgroundColor:"green"}}*/>
-         {text.map((item)=><span style={{fontSize:prop.size +"px",width:prop.size+'px',color:prop.logoColor}} className='center text-logo'onClick={handleClick} >{item}</span>)}
+      <div className='center h-md' id='text-container'   >
+         {text.map((item)=><span {...handleMouse()} style={{fontSize:prop.size +"px",width:prop.size+'px',color:prop.logoColor,position:'relative',top:mouse.y,left:mouse.x}} className='center text-logo' >{item}</span>)}
       </div>
    </div>
   )
