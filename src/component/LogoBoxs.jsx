b0e54bf9ba30a2e21219d7630d5c29eb72d54419
rekidh <89791981+rekidh@users.noreply.github.com>
2023-01-28 23:44:15 +0700

    first commit

--- src/component/LogoBoxs.jsx
+++ src/component/LogoBoxs.jsx
@@ -5,20 +5,56 @@ import {useDrag} from 'react-use-gesture'
 
 export default function LogoBoxs(prop) {
 
-  const [mouse,setMouse]=useState({x:0,y:0})
+  const [position,setPosition]=useState({x:0,y:0})
+  const [parms,setParms]=useState(null)
+  const [on,setOn]=useState('')
   const text=prop.logo?[...prop.logo]: Array.from('tulis sesuatu')
-  const handleMouse= useDrag((e)=>{
-    setMouse({
-      x:e.offset[0],
-      y:e.offset[1]
-    })
-  })
 
-  
+  const handleMouse=(e)=>{
+    setPosition({x:e.clientX,y:e.clientY})
+    handleItem({parms:parms,on:on})
+  }
+
+  const handleItem=({parms,on})=>{
+    if(parms==null){
+      return
+    }else{
+      switch(on){
+        case "down":
+            parms.style=`"
+            cursor:grabbing;
+            color:red;
+            z-index:99;
+            position:absolute;
+            top:${position.y}px;
+            left:${position.x}px;
+            "`
+          break
+        case "up":
+          parms.style=`"
+            cursor:grab;
+            "`
+      }
+    }
+  }
+  const mouseDown=(e)=>{
+    setOn('down')
+    setParms(e.target)
+  }
+  const handleUp=(e)=>{
+    setOn('up')
+    setParms(null)
+
+  }
+
   return (
-    <div className={prop.className} >
-      <div className='center h-md' id='text-container'   >
-         {text.map((item)=><span {...handleMouse()} style={{fontSize:prop.size +"px",width:prop.size+'px',color:prop.logoColor,position:'relative',top:mouse.y,left:mouse.x}} className='center text-logo' >{item}</span>)}
+    <div className={prop.className} onMouseMove={handleMouse} >
+      <div className='center h-md' id='text-container' >
+        {text.map((item)=>
+          <div  onMouseDown={mouseDown}  onMouseUp={handleUp}> 
+            <span className='center text-logo' style={{fontSize:prop.size +"px",width:prop.size+'px',color:prop.logoColor,position:"relative"}} >{item}</span>
+          </div>)
+        }
       </div>
    </div>
   )
